56b12d96c5c798eb7c3d7b7d2232ee03505ddedb
Dhruv Patel <59895228+dhruvsp@users.noreply.github.com>
2020-12-16 12:31:02 -0500

    Update Robinhood_Excel_Dataframe.py

--- Robinhood_Excel_Dataframe.py
+++ Robinhood_Excel_Dataframe.py
@@ -1,85 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
+# Install robinhood
 pip install robin_stocks
 
-
-# In[5]:
-
-
+#Required Library
 import robin_stocks 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-robin_stocks.login("dhruvspatel","Sureshdpatel1234")
+#LoggingIn into Robinhood
+robin_stocks.login("YourUserID","YourPassword")
 
+#StockList
 my_stocks = robin_stocks.build_holdings()
 
-
-# In[6]:
-
-
+#Converting into Dataframe for better Visualization
 stock_list = pd.DataFrame(my_stocks)
 stock_list = stock_list.T
 stock_list['ticker'] = stock_list.index
+
+#Dropping Unnessesary Colmun and Adding new parameter for calculation
 stock_list = stock_list.reset_index(drop=True)
 stock_list['quantity'] = stock_list['quantity'].astype(float)
 stock_list['average_buy_price'] = stock_list['average_buy_price'].astype(float)
-
-
-# In[7]:
-
-
-stock_list
-
-
-# In[8]:
-
-
 stock_list["total_stock_price"] = stock_list["quantity"]*stock_list["average_buy_price"]
-
-
-# In[9]:
-
-
-stock_list
-
-
-# In[10]:
-
-
 stock_list.loc['Total Money Invested'] = pd.Series(stock_list['total_stock_price'].sum(), index = ['total_stock_price'])
 
-
-# In[11]:
-
-
+#Converting Pandas dataframe into Excel File
 data = pd.DataFrame(dct)  
-
-
-# In[12]:
-
-
-stock_list
-
-
-# In[13]:
-
-
 stock_list.drop(['id','type'], axis=1)
-
-
-# In[14]:
-
-
 stock_list.to_excel("StockList.xlsx")
-
-
-# In[ ]:
-
-
-
-
